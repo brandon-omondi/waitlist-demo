41d7244b8c0058a1c42079d9e96a0e6453c8e910
Brandon Omondi <73080323+brandon-omondi@users.noreply.github.com>
2024-05-02 12:51:43 +0300

    typo

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -14,7 +14,7 @@ export function PageHeader () {
             </div>
             <div className="c-right items-end flex  font-poppins mt-4 gap-3 lg:gap-6">
                 {/* <div><button className="text-white text-xs md:text-sm text-pretty border-solid border-2 border-white rounded-full px-4 py-1 hover:bg-black hover:text-white">About Us</button></div> */}
-                <div><button className="text-white text-xs md:text-sm border-solid border-2 border-white rounded-full px-4 py-1 hover:bg-black hover:text-white" onClick={openGoogleForm}>Questionaire</button></div>
+                <div><button className="text-white text-xs md:text-sm border-solid border-2 border-white rounded-full px-4 py-1 hover:bg-black hover:text-white" onClick={openGoogleForm}>Questionnaire</button></div>
                 {/* <div><button className="text-black text-xs md:text-sm text-pretty border-solid border-2 border-white rounded-full px-4 py-1 bg-white hover:bg-black hover:text-white">Join Beta</button></div> */}
             </div>
         </div>
