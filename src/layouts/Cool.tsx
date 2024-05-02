235cac3b1eac98aa1c3179047de8a81cd27e1d5c
Brandon Omondi <73080323+brandon-omondi@users.noreply.github.com>
2024-05-02 11:23:18 +0300

    m

--- src/layouts/Cool.tsx
+++ src/layouts/Cool.tsx
@@ -7,12 +7,12 @@ export function Cool() {
                     <h3 className="font-semibold md:text-[28px] mb-3">Save money</h3>
                     <p className="text-left text-xs md:text-sm leading-6 mb-7 opacity-80">With studentribe, every purchase is a saving. We’ve lobbied for exclusive student discounts from the businesses you love, spanning 
                     eateries, retailers, travel and accomodation, and many other industries.</p>
-                    <div className="bg-orange-500 text-white font-normal leading-6 text-sm rounded-3xl w-fit text-nowrap px-4 py-2 mb-6">cheaper than anywhere else</div>
+                    <div className="bg-orange-500 text-white font-normal leading-6 text-xs rounded-3xl w-fit text-nowrap px-4 py-2 mb-6">cheaper than anywhere else</div>
                 </div>
                 <div className="c-community bg-cards md:w-[420px] h-full px-5 pt-3 pb-4 rounded-3xl text-justify">
                     <h3 className="font-semibold md:text-[28px] mb-3">Community</h3>
                     <p className="text-left text-xs md:text-sm leading-6 mb-7 opacity-80">We offer exciting spaces for community building through music concerts, networking mixers and many more.</p>
-                    <div className="bg-indigo-500 text-white font-normal leading-6 text-sm rounded-3xl w-fit text-nowrap px-4 py-2 mb-6">meet, connect & grow</div>
+                    <div className="bg-indigo-500 text-white font-normal leading-6 text-xs rounded-3xl w-fit text-nowrap px-4 py-2 mb-6">meet, connect & grow</div>
                 </div>
             </div>
         </div>
