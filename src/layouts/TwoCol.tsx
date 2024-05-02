8a47273c7f9480c4eebc80a49a1e6444beb23b6f
Brandon Omondi <73080323+brandon-omondi@users.noreply.github.com>
2024-05-02 08:07:32 +0300

    right margin on two-col

--- src/layouts/TwoCol.tsx
+++ src/layouts/TwoCol.tsx
@@ -28,11 +28,11 @@ export function TwoCol() {
 
     return(
         <div className="c-container flex flex-col md:flex-row gap-2 mt-2 w-full md:h-[650px]">
-            <div className="c-leftside w-1/2 mt-10">
+            <div className="c-leftside w-1/2 mt-10 mr-5 md:mr-0 ">
                 <div className="text-[28px] md:text-[56px] font-bold text-pretty">
                     <h1 className='leading-[36px] md:leading-[60px] text-white font-poppins text-pretty'>Something new <br/><span className="opacity-40 text-pretty">for every student</span></h1>
                 </div>
-                <p className="mt-4 w-[444px] text-white text-justify text-sm md:text-xl font-poppins font-normal leading-6 opacity-60">Our team has been working on an exciting product to make spending much cheaper for students, forever!
+                <p className="mt-4 w-[444px] text-white text-justify text-lg md:text-xl font-poppins font-normal leading-6 opacity-60">Our team has been working on an exciting product to make spending much cheaper for students, forever!
 
 And now, you can become the very first user and get lifetime student discounts for free.</p>
                 <div className="flex items-center font-poppins gap-2 mt-8" id="2">
